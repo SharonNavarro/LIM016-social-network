ec289d187057a37f6a1d645d45b8acc8cadc4cb8
Milagros_Leon_Cueva <milagros.lc2021@gmail.com>
2021-12-13 17:41:33 -0500

    Login de Facebook

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,23 @@
-// aqui exportaras las funciones que necesites
 
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
-};
+export const loginTemplate = () => {
+  return `<div class="containerInputs">
+  <img class="logoLogin" src="./images/logoNetcoins.png" alt="">
+  <h1 >Netcoins</h1>
+  <input class="inputUser" id="inputUser"type="text" placeholder="Usuario">
+  <input class="inputPassword" id="inputPassword" type="text" placeholder="Contraseña">
+  <button class="btn third" id= "btnLogin">LogIn</button>
+  <label class="lbl" for=""> o ingresa con:</label>
+  <div class="loginIcons">    
+   <a id="loginFacebook"><img  src="./images/logo-facebook.png" alt=""></a> 
+    <a id="loginGmail"><img  src="./images/logo-gmail.png" id="authGoogle" alt=""></a>
+     <a id="loginTwitter"><img  src="./images/icono-twitter.png" alt=""></a>
+     </div>
+     <div class="groupLbl">  
+     <label class="lblCuenta" for="">¿No tienes una cuenta?</label>
+     <label class="lblRegistrate" for="">Registrate</label>
+     <button class="btn third" id= "btnLogout">LogIn</button>
+     </div>
+  
+  </div>`;
+}
+
--- src/main.js
+++ src/main.js
@@ -1,5 +1,105 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import { loginTemplate} from './lib/index.js';
+// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+ import { getAuth, FacebookAuthProvider, GoogleAuthProvider, signInWithRedirect, getRedirectResult } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
 
-myFunction();
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyDLn-gLtWbPB0uo4YeVleQHoU--dUGFIjA",
+   authDomain: "social-network-netcoins.firebaseapp.com",
+   databaseURL: "https://social-network-netcoins-default-rtdb.firebaseio.com",
+   projectId: "social-network-netcoins",
+   storageBucket: "social-network-netcoins.appspot.com",
+   messagingSenderId: "359714878827",
+   appId: "1:359714878827:web:1856985dbf41196a7b882e"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ const auth= getAuth();
+
+// aqui exportaras las funciones que necesites
+const login = document.getElementById('login');
+
+login.innerHTML = loginTemplate();
+
+const btnLogout = document.getElementById("btnLogout");
+
+btnLogout.addEventListener('click',()=>{
+    auth.signOut().then(()=>{
+        console.log("saliste");
+    })
+})
+
+/* btnLogin.addEventListener('click', loginEmail); */
+
+/* function loginEmail() {
+    const email = document.getElementById("inputUser").value;
+    const password = document.getElementById("inputPassword").value;
+    auth.
+        createUserWithEmailAndPassword(email, password)
+        .then(userCredential => {
+            console.log("siiii");
+        });
+
+} */
+
+let loginGmail = document.getElementById("loginGmail");
+
+const loginGoogle = () => {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  signInWithRedirect(auth, provider);
+  getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+  });
+}
+loginGmail.addEventListener("click", loginGoogle, false)
+
+/*------AUTH WITH FACEBOOK------*/
+let loginFacebook = document.getElementById("loginFacebook");
+
+const loginAppFacebook = () => {
+  const auth = getAuth();
+  const provider = new FacebookAuthProvider();
+  signInWithRedirect(auth, provider);
+  getRedirectResult(auth)
+  .then((result) => {
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    const credential = FacebookAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    const user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // AuthCredential type that was used.
+    const credential = FacebookAuthProvider.credentialFromError(error);
+    // ...
+  });
+}
+loginFacebook.addEventListener("click", loginAppFacebook, false)
