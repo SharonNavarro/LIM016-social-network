4af40d87c5727c975abfb0a458e96d23b1af88ae
JenyferMarquez <favimarqez3@gmail.com>
2021-12-13 22:00:09 -0500

    main

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,22 +1,20 @@
-
 export const loginTemplate = () => {
-return `
-<div class="containerInputs">
-  <img class="logoLogin" src="./images/logoNetcoins.png" alt="">
-  <h1 >Netcoins</h1>
-  <input class="inputUser" id="inputUser"type="text" placeholder="Usuario">
-  <input class="inputPassword" id="inputPassword" type="text" placeholder="Contraseña">
-  <button class="btn third" id= "btnLogin">LogIn</button>
-  <label class="lbl" for=""> o ingresa con:</label>
-  <div class="loginIcons">    
-  <a id="loginFacebook"><img  src="./images/logo-facebook.png" alt=""></a> 
-  <a id="loginGmail"><img  src="./images/logo-gmail.png" id="authGoogle" alt=""></a>
-  <a id="loginTwitter"><img  src="./images/icono-twitter.png" alt=""></a>
+return `<div class="containerInputs">
+<img class="logoLogin" src="./images/logoNetcoins.png" alt="">
+<h1 >Netcoins</h1>
+<input class="inputUser" id="inputUser"type="text" placeholder="Usuario">
+<input class="inputPassword" id="inputPassword" type="text" placeholder="Contraseña">
+<button class="btn third" id= "btnLogin">LogIn</button>
+<label class="lbl" for=""> o ingresa con:</label>
+<div class="loginIcons">    
+<a id="loginFacebook"><img  src="./images/logo-facebook.png" alt=""></a> 
+<a id="loginGmail"><img  src="./images/logo-gmail.png" id="authGoogle" alt=""></a>
+<a id="loginTwitter"><img  src="./images/icono-twitter.png" alt=""></a>
 </div>
-     <div class="groupLbl">  
-     <label class="lblCuenta" for="">¿No tienes una cuenta?</label>
-     <label class="lblRegistrate" for="">Registrate</label>
-     <button class="btn third" id= "btnLogout">LogIn</button>
-     </div> 
-  </div>`;
+<div class="groupLbl">  
+<label class="lblCuenta" for="">¿No tienes una cuenta?</label>
+<label class="lblRegistrate" for="">Registrate</label>
+<button class="btn third" id= "btnLogout">LogIn</button>
+</div>
+</div>`;
 }
