6be59df3c4e127a917f0e14f4269e34931a0f97f
Milagros_Leon_Cueva <milagros.lc2021@gmail.com>
2021-12-23 10:49:21 -0500

    solucionando menu

--- src/view/Home.js
+++ src/view/Home.js
@@ -20,77 +20,6 @@ export default () => {
    <p>Camila</p>
  </div>
 
-<<<<<<< HEAD
- </div>
-<div class="alignCenter">
-<div class="containerWriter">
- 
- <div class="userProfile">
-   <img src="./images/profile4.png">
-   <div>
-     <p>Jhoana Durand</p>
-   </div>
- </div> 
-
- <div class="containerPost">
-   <textarea rows="3" placeholder="What's on your mind, Jhoana?"></textarea>
-   
-   <div class="containerIconsBtn">
-     <div class="addPost">
-       <a href="#"><img src="./images/iconPhoto.png" alt= ></a>
-       <a href="#"><img src="./images/iconPrivacity.png" alt= ></a>
-       <button class="postBtn">Go!</button>
-     </div>
-   </div>
- </div>
-</div>
-
-   <div class="containerPosts">
-     <div class="userProfile">
-     <img src="./images/profile2.png">
-       <div>
-         <p>Carlos Tarazona</p>
-         <span>June 24 2021, 13:40 pm</span>
-       </div>
-     </div> 
-
-     <p>Bienvenidxs a la experiencia Netcoins!! Contrary to popular 
-     belief, Lorem Ipsum is not simply random text.
-     It has roots in a piece of classical Latin.</p>
-     
-     <div class="postRow">
-
-       <div class="activityIcons">
-         <div><img src="./images/heartIcon.png">1508</div>
-         <div><img src="./images/textGlobeIcon.png"></div>
-       </div>
-
-     </div>
-   </div>
-
-   <div class="containerPosts">
-     <div class="userProfile">
-     <img src="./images/profile1.png">
-       <div>
-         <p>Sabrina Castañeda</p>
-         <span>June 24 2021, 14:10 pm</span>
-       </div>
-     </div> 
-
-     <p>It has roots in a piece of classical Latin.</p>
-     
-     <div class="postRow">
-
-       <div class="activityIcons">
-         <div><img src="./images/heartIcon.png">1508</div>
-         <div><img src="./images/textGlobeIcon.png"></div>
-       </div>
-
-     </div>
-   </div>
- </div>
-    
-=======
     </div>
  
 <div class="alignCenter">
@@ -240,7 +169,6 @@ export default () => {
         </div>
     </div>
 
->>>>>>> 9c8a8fb95d09d4778d2f7f4e8dc77677bef9fb33
     </div>
     </div>
  `;
