8825a7d39d1423a3e15deb4a04006fc6a945e6c6
JenyferMarquez <favimarqez3@gmail.com>
2022-01-03 09:47:00 -0500

    trayendo cambios

--- src/view/Register.js
+++ src/view/Register.js
@@ -4,34 +4,87 @@ import{ auth } from "../firebase/config.js"
 
 export default () => {
     const viewRegister = `
-<div class="containerLogin">
+    <form action="" class="containerLogin" id="formulario">
   <div class="sectionWelcome">
   </div>
 
   <div class="sectionLogin" id="sectionLogin">
     <div class="login" id=login>
 
-    <div class="register" id=register>  
-    <div class="containerInputs">
+    <div class="register" id=register> 
+    <div class="loginImg">
     <img class="logoLogin" src="./images/logoNetcoins.png" alt="">
-    <h1 >Netcoins</h1>
-    <input class="inputUser" id="inputUserRegister"type="text" placeholder="Usuario">
-    <input type="password" class="inputPassword" id="inputPasswordRegister" type="text" placeholder="Contraseña">
-    <button class="btn third" id= "btnRegister">Registrarse</button>
+    </div>
+    <h1 >Netcoins</h1> 
+
+    <div class="containerInputs">
+
+      <div class="formulario__grupo" id="grupo__usuario">
+				<div class="formulario__grupo-input">
+          <input class="formulario__input" id="inputUserRegister"type="text" name="usuario" placeholder="Usuario" >
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">El usuario tiene que ser de 4 a 16 dígitos y solo puede contener numeros, letras y guion bajo.</p>
+			</div>
+
+      <div class="formulario__grupo" id="grupo__nombre">
+				<div class="formulario__grupo-input">
+          <input class="formulario__input" id="inputUserName"type="text" name="nombre" placeholder="Nombre" required >					
+          <i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+        <p class="formulario__input-error">El usuario tiene que ser de 4 a 16 dígitos y solo puede contener numeros, letras y guion bajo.</p>
+			</div>
+        <div class="formulario__grupo-input">
+          <input class="formulario__input" id="inputUserLastName"type="text" name="apellido" placeholder="Apellido" required >				
+          <i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">El usuario tiene que ser de 4 a 16 dígitos y solo puede contener numeros, letras y guion bajo.</p>
+        <div class="formulario__grupo" id="grupo__correo">
+          <div class="formulario__grupo-input">
+            <input class="formulario__input" id="inputUserEmail"type="email" name="email" placeholder="Email" > 
+            <i class="formulario__validacion-estado fas fa-times-circle"></i>
+          </div>
+          <p class="formulario__input-error">El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
+        </div>
+      <div class="formulario__grupo" id="grupo__password">
+				<div class="formulario__grupo-input">
+				  <input type="password" class="formulario__input" id="inputPasswordRegister" name="password" type="text" placeholder="Contraseña nueva" >
+					<i class="formulario__validacion-estado fas fa-times-circle"></i>
+				</div>
+				<p class="formulario__input-error">La contraseña tiene que ser de 4 a 12 dígitos.</p>
+			</div>
+    
+      <div class="formulario__grupo" id="grupo__terminos">
+				<label class="terminos">
+					<input class="terminosCheckbox" type="checkbox" name="terminos" id="terminos" >
+					Acepto los Terminos y Condiciones
+				</label>
+		</div>
+    <div class="formulario__mensaje" id="formulario__mensaje">
+    <p class="texto">¿Ya tienes una cuenta? <a id="registrate" href="#/Login"> Iniciar Sesión</a></p> 
+		</div>
+    <button type= "submit" class="btn third" id= "btnRegister">Registrarse</button>
+    
      <div class="loginIcons">    
       </div>  
      </div>
     </div>
     </div>
+    </form>
   </div>
-</div>`;
+</div>
+`;
 
+
+   
     const divElemt = document.createElement('section');
     divElemt.classList.add('classViewRegister')
     divElemt.innerHTML = viewRegister;
 
     const email= divElemt.querySelector("#inputUserRegister");
     const password= divElemt.querySelector("#inputPasswordRegister");
+    const name= divElemt.querySelector("#inputUserName");
+    
 
     const btnRegister= divElemt.querySelector("#btnRegister")
     btnRegister.addEventListener("click",()=>{    
