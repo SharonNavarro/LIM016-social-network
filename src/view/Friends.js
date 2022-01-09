9d9795aa09f1a3259b2f297973c8444e215331fe
JenyferMarquez <favimarqez3@gmail.com>
2022-01-09 17:34:33 -0500

    main

--- src/view/Friends.js
+++ src/view/Friends.js
@@ -1,7 +1,31 @@
 export default () => {
   const viewFriends = `
-    <div class="content" > 
-    <p>bienvenido a frienss</p>
+  
+    <div class="content friends" > 
+      <p>bienvenido a friends</p>
+          <div class="container">
+          <div class="seccion-buscar">
+          <div class="input-buscar">
+              <input type="search" placeholder="Buscar usuario">
+              <i class="fas fa-search"></i>
+          </div>
+             </button>
+             <div class="friends">
+             <button type="button" class="btn-animado animacion-uno">
+             <i class="fas fa-user-plus"></i>
+             <span class="tex-icono">
+             Solicitudes de Amistad
+             </span>
+            </button>
+               
+            <button type="button" class="btn-animado animacion-dos">
+            <i class="fas fa-users"></i>
+            <span class="tex-icono">
+            Todos tus amigos 
+            </span>
+            </button>
+           </div>       
+        </div>
      </div>`;
 
   const divElemt = document.createElement('section');
