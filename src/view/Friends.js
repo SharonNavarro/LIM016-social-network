d8d29fec96806dcfbc83ca13d352aa84023c3c7c
SharonNavarro <shanmowrk@gmail.com>
2022-01-25 20:09:53 -0500

    Creacion del campo followed y su implementacion

--- src/view/Friends.js
+++ src/view/Friends.js
@@ -1,46 +1,97 @@
+import { 
+  templateViewFriends,
+  templateFriend
+} from "./templates/templateFriends.js";
+
+import { userState } from "../firebase/auth.js"
+
 export default () => {
-  const viewFriends = `
- 
-  <div class="contentFriends" > 
-
-    <div class="content">
-
-      <div class="seccion-buscar-Friends">
-        <div class="searchBar">
-          <input type="search" class="search" placeholder="Buscar usuario">
-          <i class="fas fa-search"></i>
-        </div>      
-         
-        <button type="button" class="btn-animado animacion-dos">
-          <i class="fas fa-users"></i>
-          <span class="tex-icono">
-            Todos tus amigos 
-          </span>
-        </button>
-      </div>
-       
-      <div class ="containerAllFriends">
-        
-        <div class="myFriend">
-          <div class="boxMyFriend"><img src="images/profile1.png" id ="imgMyFriend"></div>
-          <div class="boxName">
-            <h3 class="nameFriend">Johanna Smith</h3>
-            <p class="nameFriend">Seguidores:10</p>
-          </div>
-        </div> 
-
-      </div>
-
-    </div>
-
-  </div>
-  `;
+  
+  const viewFriends= templateViewFriends;
 
   const divElemt = document.createElement("section");
   divElemt.classList.add("position");
   divElemt.innerHTML = viewFriends;
 
-  
+  userState(async (user) => {
+
+    if (user) {
+      // displayName = user.displayName;
+      // photoURL = user.photoURL;
+      // email = user.email;
+      // nameUser.innerHTML = displayName;
+      // photoUser.src = photoURL;
+      // userid = user.uid;
+      showFollowed();
+    }
+  })
+
+  let idUsuarioLogin, followed, querySnapshot, post, idPosts, contentPosts, dateOfPublish, hourPublish, userName, urlPhoto, showFollowed;
+
+  showFollowed = async () => {
+    getFileAdd=""
+    await getIdUsers();
+    async function getIdUsers() {
+      const querySnapshot = await getUsers();
+      querySnapshot.forEach((doc) => {
+        if (displayName == doc.data().nameUser) {
+          idUsuarioLogin = doc.data().idUser;
+          followed = doc.data().followed;
+        }
+      });
+    }
+    let imagenAdd;
+    let contStars = [];
+    let contHearts = [];
+    querySnapshot = await getPublishes();
+    let templatePosts = "";
+    querySnapshot.forEach((doc) => {
+      post = doc.data();
+      post.id = doc.id;
+      idPosts = post.id;
+      contentPosts = doc.data().content;
+      dateOfPublish = doc.data().datePublish;
+      hourPublish = doc.data().hourPublish;
+      userName = doc.data().userName;
+      urlPhoto = doc.data().urlPhoto;
+      contStars = doc.data().likesPost;
+      imagenAdd = doc.data().imagen;
+      contHearts = doc.data().hearts;
+
+      let btnFollowUsers;
+      
+      (followed.indexOf(idUsuarioLogin) !==-1)? btnFollowUsers.innerHTML="Seguido" : btnFollowUsers.innerHTML=""; 
+
+      if (btnFollowUsers.innerHTML = "Seguido") {
+        templatePosts += templatePublishes(userName, urlPhoto, idPosts, contentPosts, dateOfPublish, hourPublish, contStars.length, iconStars, imagenAdd, iconHearts, contHearts.length);
+
+      }
+
+    });
+
+    postContainer.innerHTML = templatePosts;
+
+    const btnFollow = document.querySelectorAll(".btnFollow");
+    //--------------------------------------------
+
+    btnFollow.forEach((btn) => {
+      btn.addEventListener("click", async (e) => {
+        const idPost = e.target.dataset.id;
+        if (e.target.classList.contains('')) {
+          desFollow(idPost, idUsuarioLogin).FieldValue;
+          console.log("dejo de seguir");
+          await showFollowed();
+        } else {
+          inFollow(idPost, idUsuarioLogin).FieldValue;
+          // e.target.classList.add('')
+          btn.innerHTML="Seguido";
+          console.log("empezo a segurilo");
+          await showFollowed();
+        }
+      })
+    })
+  }
+
 
 
   return divElemt;
